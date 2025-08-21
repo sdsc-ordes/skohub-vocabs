83e952c3950c0d2a02418178df52a8b27d9a3323
rmfranken <robin.franken@epfl.ch>
2025-08-21 11:49:59 +0200

    fix:syntax error

--- src/queries.js
+++ src/queries.js
@@ -111,7 +111,7 @@ module.exports.allConcept = (inScheme, languages) => `
             id
           }
           exactMatch {
-            id-
+            id
           }
           inScheme {
             id
